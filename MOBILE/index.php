28c939e7a0af906b9b9a4a6bddd62e6d32c1baf4
Seonhye yang <yangseonhye@yangseonhyeui-iMac.local>
2015-05-29 00:25:27 +0900

    인덱스 이미지 수정

--- MOBILE/index.php
+++ MOBILE/index.php
@@ -1,6 +1,6 @@
 <!doctype html>
 <head>
-  <title>VDL KAKAO FRIENDS</title>
+  <title>VDL</title>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0" />
@@ -70,7 +70,7 @@ html,body { height: 100%; margin: 0; padding: 0;}
     </div>
     
     <div class="block_top">
-    	<a href="#"><img src="images/img_top.png" alt=""/></a>
+    	<a href="#"><img src="images/img_top.jpg" alt=""/></a>
     </div>
     
     <div class="block_mina">
