9adccd2ec9d74e8e6cc252fd6a92023e97c17484
kyhfan <yh.kim@minivertising.kr>
2015-06-02 01:31:33 +0900

    db 커넥션 수정

--- include/dbi.php
+++ include/dbi.php
@@ -8,7 +8,8 @@
      * Created : 2014
      *
      ******************************************************************************/
-	$my_db = new mysqli("localhost", "root", "m!nv#Rtisin9", "vdl_metal");
+	//$my_db = new mysqli("localhost", "root", "m!nv#Rtisin9", "vdl_metal");
+	$my_db = new mysqli("10.3.0.121", "root", "m!nv#Rtisin9", "vdl_metal");
 	if (mysqli_connect_error()) {
 		exit('Connect Error (' . mysqli_connect_errno() . ') '. mysqli_connect_error());
 	}
