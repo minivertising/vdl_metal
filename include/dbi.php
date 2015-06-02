3856b0e9604af76d0cfdb0169d318249240b3466
kyhfan <yh.kim@minivertising.kr>
2015-06-02 16:22:21 +0900

    11

--- include/dbi.php
+++ include/dbi.php
@@ -9,7 +9,7 @@
      *
      ******************************************************************************/
 	//$my_db = new mysqli("localhost", "root", "m!nv#Rtisin9", "vdl_metal");
-	$my_db = new mysqli("10.3.0.121", "root", "m!nv#Rtisin9", "vdl_metal");
+	$my_db = new mysqli("10.3.0.123", "root", "m!nv#Rtisin9", "vdl_metal");
 	if (mysqli_connect_error()) {
 		exit('Connect Error (' . mysqli_connect_errno() . ') '. mysqli_connect_error());
 	}
