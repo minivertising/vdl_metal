426db02eccb139343c9e84ab46aa6d646c99a9bc
kyhfan <yh.kim@minivertising.kr>
2015-06-04 12:46:14 +0900

    테스트

--- MOBILE/index2.php
+++ MOBILE/index2.php
@@ -1,4 +1,13 @@
 <?
 	include_once   "./header.php";
+
+	if (!$_SESSION['ss_media'])
+	{
+		$media	= $_REQUEST['media'];
+echo $media;
+		$_SESSION['ss_media'] = $media;
+		//VM_InsertTrackingInfo($media, $gubun);
+	}
+
 ?>
 <a href="popup_event1.php">이벤트 참여</a>
