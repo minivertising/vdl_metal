759e893256d12c44dc6d0123e3695b2e97a3c91d
kyhfan <yh.kim@minivertising.kr>
2015-06-04 13:46:45 +0900

    팝업 오픈

--- PC/index.php
+++ PC/index.php
@@ -43,7 +43,7 @@
         	<a href="#" onclick="sns_share('story');"><img src="images/btn_sns_ks.png" alt=""/></a>
             <a href="#" onclick="sns_share('twitter');"><img src="images/btn_sns_tw.png" alt=""/></a>
         </div>
-        <div class="kakao" id="kakao_banner" style="display:none;">
+        <div class="kakao" id="kakao_banner">
         	<div class="inner">
             	<div class="close"><a href="#" onclick="$('#kakao_banner').hide();return false;"><img src="images/btn_close_pop.png" alt=""/></a></div>
         		<div class="img"><a href="http://www.vdlxkakao.com/?media=vdl_metal" target="_blank"><img src="images/btn_kakao.png" alt=""/></a></div>
